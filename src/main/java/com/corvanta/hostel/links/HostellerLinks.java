0191a108585b3261c5fa130190e66e69bd5f13f4
JavidNashraf <javidnashraf@gmail.com>
2022-09-25 15:02:49 +0530

    Corvanta Hostel Update

--- src/main/java/com/corvanta/hostel/links/HostellerLinks.java
+++ src/main/java/com/corvanta/hostel/links/HostellerLinks.java
@@ -8,5 +8,6 @@ public class HostellerLinks {
 	public static final String LIST_HOSTELLERS = "/hostellers";
     public static final String ADD_HOSTELLER = "/hosteller";
     public static final String GET_HOSTELLER = "/hosteller/{id}"; 
+    public static final String UPDATE_HOSTELLER = "/hosteller/{id}";
 
 }
