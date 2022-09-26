22df79947fbaa03e40a7171999819081316bc545
JavidNashraf <112257696+JavidNashraf@users.noreply.github.com>
2022-09-26 14:40:22 +0530

    Room Requests updated in Corvanta Hostel Project

--- src/main/java/com/corvanta/hostel/links/HostellerLinks.java
+++ src/main/java/com/corvanta/hostel/links/HostellerLinks.java
@@ -4,14 +4,19 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class HostellerLinks {
-	
+
 	public static final String LIST_HOSTELLERS = "/hostellers";
-    public static final String ADD_HOSTELLER = "/hosteller";
-    public static final String GET_HOSTELLER = "/hosteller/{id}"; 
-    public static final String UPDATE_HOSTELLER = "/hosteller/{id}";
-    public static final String LIST_ROOMDETAILS = "/roomDetails";
-    public static final String ADD_ROOMDETAILS = "/roomDetail";
-    public static final String GET_ROOMDETAILS = "/roomDetail/{id}";
-    public static final String UPDATE_ROOMDETAILS = "/roomDetail/{id}";
+	public static final String ADD_HOSTELLER = "/hosteller";
+	public static final String GET_HOSTELLER = "/hosteller/{id}";
+	public static final String UPDATE_HOSTELLER = "/hosteller/{id}";
+	public static final String LIST_ROOMDETAILS = "/roomDetails";
+	public static final String ADD_ROOMDETAILS = "/roomDetail";
+	public static final String GET_ROOMDETAILS = "/roomDetail/{id}";
+	public static final String UPDATE_ROOMDETAILS = "/roomDetail/{id}";
+	public static final String LIST_REQUESTS = "/roomrequests";
+	public static final String ADD_REQUEST = "/roomrequest";
+	public static final String GET_REQUEST = "/roomrequest/{id}";
+	public static final String UPDATE_REQUEST = "/roomrequest/{id}";
+
+
 }
- 
