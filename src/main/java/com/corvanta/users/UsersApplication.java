e9628ace1622a244a6332a0fb7eed791e3322afb
Azar <azarudeenpersonnal@gmail.com>
2023-04-29 17:00:32 +0530

    Enabling CORS.

--- src/main/java/com/corvanta/users/UsersApplication.java
+++ src/main/java/com/corvanta/users/UsersApplication.java
@@ -2,7 +2,10 @@ package com.corvanta.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan({"com.corvanta.users","com.corvanta.hostel"})
@@ -11,5 +14,14 @@ public class UsersApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UsersApplication.class, args);
 	}
-
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
+	}
 }
